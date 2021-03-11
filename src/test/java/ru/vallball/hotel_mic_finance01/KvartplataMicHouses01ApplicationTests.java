bcc27ce0cc99a0b896a503d8277afb1973378987
vall-ball <vall-ball@yandex.ru>
2021-03-11 15:10:25 +0700

    commit 11.03.2021

---
+++ src/test/java/ru/vallball/hotel_mic_finance01/KvartplataMicHouses01ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vallball.hotel_mic_finance01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KvartplataMicHouses01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
